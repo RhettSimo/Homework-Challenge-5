132f7e89d08e742a32f71021540fda6dbdfcfbed
RhettSimo <simonsonrhett1337@gmail.com>
2023-04-18 15:18:28 -0500

    added the rest of the time blocks and some other necessary functions in js

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
+var eventsData;
+
 function setHourColors() {
     var now = dayjs();
     
-    for (var i =9; i < 18; i++) {
+    for (var i = 9; i < 18; i++) {
+        // $("#hour-" + i + " textarea").removeClass("future");
         if (i < now.hour()) {
-            $("#hour" + i + " textarea").addClass("past");
+            $("#hour-" + i + " textarea").addClass("past");
         } else if (i == now.hour()) {
-            $("#hour" + i + " textarea").addClass("present");     
+            $("#hour-" + i + " textarea").addClass("present");     
+        } else if (i > now.hour()) {
+            $("#hour-" + i + " textarea").addclass("future");
         }
     }
 }
 
+function loadStoredData() {
+    var eventsData =  JSON.parse(localStorage.getItem("calendarEvents"));
+    if (!eventsData) (
+        eventsData = {
+            hour9: "",
+            hour10: "",
+            hour11: "",
+            hour12: "",
+            hour13: "",
+            hour14: "",
+            hour15: "",
+            hour16: "",
+            hour17: "",
+        }
+    )
+}
+
+function handleSaveClick() {
+    // grab data from html
+    var hourBlock = $(event.target).parent();
+    var value = hourBlock.children("textarea").val();
+    var hour = hourBlock.attr('id').split("-")[1];
+
+    // modify data object
+    eventsData["hour" + hour] = value;
+    
+    // store data in local storage
+    localStorage.setItem("calendarEvents", JSON.stringify(eventsData));
+}
+
+$('.saveBtn').on('click', handleSaveClick);
+
 $(function() {
+    loadStoredData();
     setHourColors();
 });
